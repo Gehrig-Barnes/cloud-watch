db2a93c354e63b192dd8d63d803d729f758b13ac
gehrig <gehrigbarnes@gmail.com>
2022-11-19 08:20:37 -0500

    refactoring my code. Created seperate files for my js

---
+++ src/index.js
@@ -0,0 +1,60 @@
+//long term ideas. Have the user be able to change the units from imperial to Metrics system.
+//also the user should have the option to change the language.
+
+const currentPage = window.location.pathname;
+const closeButton = document.querySelector(".close-button");
+const modal = document.querySelector(".modal");
+
+const navLinks = document.querySelectorAll("nav a").forEach((link) => {
+  if (link.href.includes(`${currentPage}`)) {
+    link.className = "active";
+  }
+});
+
+fetch("http://localhost:3000/news")
+  .then((r) => r.json())
+  .then((data) => {
+    renderArticles(data);
+  });
+
+const articlesDiv = document.querySelector("#articles");
+
+function renderArticles(articles) {
+  if (currentPage.includes("index.html")) {
+    articles.forEach((article) => {
+      const card = document.createElement("div");
+      card.className = "card";
+      const titleTag = document.createElement("h1");
+      titleTag.textContent = article.title;
+      titleTag.className = "title";
+      const imgTag = document.createElement("img");
+      imgTag.src = article.image;
+      const pSummary = document.createElement("p");
+      pSummary.className = "summary";
+      pSummary.textContent =
+        article.summary.length > 100
+          ? `${article.summary.substring(0, 100)} ...`
+          : article.summary;
+      const pAuthor = document.createElement("p");
+      pAuthor.textContent = article.author;
+      pAuthor.className = "author";
+      const a = document.createElement("a");
+      a.href = article.source;
+      a.textContent = "Source";
+      // showFullSummaryOnClick(card, article)
+      pSummary.append(pAuthor);
+      card.append(titleTag, imgTag, pSummary, a);
+      articlesDiv.append(card);
+    });
+  }
+}
+
+function showFullSummaryOnClick(card, article) {
+  card.addEventListener("click", () => {
+    modal.classList.toggle("show-modal");
+  });
+}
+
+closeButton.addEventListener("click", () => {
+  modal.classList.toggle("show-modal");
+});
---
+++ src/weather.js
@@ -0,0 +1,48 @@
+const currentPage = window.location.pathname;
+
+const navLinks = document.querySelectorAll("nav a").forEach((link) => {
+  if (link.href.includes(`${currentPage}`)) {
+    link.className = "active";
+  }
+});
+const show = document.querySelector("#show-forecast");
+const searchForm = document.querySelector("#search-form");
+const pParent = document.querySelector("#hold-p");
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const zipCode = e.target["search-input"].value;
+  const key = "d2f2d4fe77ce13ef79d20c7db14f8e5d";
+  const getWeather = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=${key}`;
+  fetch(getWeather)
+    .then((r) => r.json())
+    .then((data) => {
+      getCurrentWeather(data.lat, data.lon, key);
+
+      if (show.textContent === "show forecast") {
+        show.addEventListener("click", () => {
+          getWeatherForecast(data.lat, data.lon, key);
+        });
+      }
+    });
+});
+
+function getCurrentWeather(lat, lon, key) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`
+  )
+    .then((r) => r.json())
+    .then((data) => console.log(data));
+}
+
+function getWeatherForecast(lat, lon, key) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`
+  )
+    .then((r) => r.json())
+    .then((data) => {
+      console.log(data);
+      pParent.removeChild(show);
+      console.log(pParent);
+    });
+}
